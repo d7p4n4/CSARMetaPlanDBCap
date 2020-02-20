b1b3d3962d319ad4e646f895b89a888d8e4e9212
d7p4n4 <bercikezs199999999@gmail.com>
2020-02-20 15:34:33 +0100

    1.2020020.4

--- CSARMetaPlanDBCap/ARMetaPlanDBCapMethods.cs
+++ CSARMetaPlanDBCap/ARMetaPlanDBCapMethods.cs
@@ -1,6 +1,7 @@
 ﻿using CSARMetaPlan.Class;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -19,146 +20,95 @@ namespace CSARMetaPlanDBCap
             Connection = connection;
         }
 
-        public void SaveAdatElem(AdatElem adatElem)
-        {
-                using (var context = new MetaPlanContext())
-                {
-
-                    context.AdatElemek.Add(adatElem);
-                    context.SaveChanges();
-                }
-
-        }
-
-        public void SaveAdatKor(Adatkor adatKor)
-        {
-                using (var context = new MetaPlanContext())
-                {
-
-                    context.AdatKorok.Add(adatKor);
-                    context.SaveChanges();
-                }
-
-        }
-
-        public void SaveEljarasTipus(EljarasTipus eljarasTipus)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.EljarasTipusok.Add(eljarasTipus);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveFejlesztesiAllapot(FejlesztesiAllapot fejlesztesiAllapot)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.FejlesztesiAllapotok.Add(fejlesztesiAllapot);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyo(Kepernyo kepernyo)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Kepernyok.Add(kepernyo);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyoAdatkor(KepernyoAdatkor kepernyoAdatkor)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.KepernyoAdatkorok.Add(kepernyoAdatkor);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyoElem(KepernyoElem kepernyoElem)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.KepernyoElemek.Add(kepernyoElem);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyoFejlesztesiAllapot(KepernyoFejlesztesiAllapot kepernyoFejlesztesiAllapot)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.KepernyoFejlesztesiAllapotok.Add(kepernyoFejlesztesiAllapot);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyoGyujtemeny(KepernyoGyujtemeny kepernyoGyujtemeny)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.KepernyoGyujtemenyek.Add(kepernyoGyujtemeny);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveKepernyoTeszteles(KepernyoTeszteles kepernyoTeszteles)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.KepernyoTesztelesek.Add(kepernyoTeszteles);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveMetodus(Metodus metodus)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Metodusok.Add(metodus);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveModul(Modul modul)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Modulok.Add(modul);
-                context.SaveChanges();
-            }
-
-        }
-
-        public void SaveMuvelet(Muvelet muvelet)
+        public void InsertMuvelet(Muvelet muvelet)
         {
             using (var context = new MetaPlanContext())
             {
 
                 context.Muveletek.Add(muvelet);
                 context.SaveChanges();
+            }
+
+        }
+
+        public bool IsExistMuveletById(Muvelet muvelet)
+        {
+            using (var context = new MetaPlanContext())
+            {
+                Muvelet isExistMuvelet = context.Muveletek.Find(muvelet.Id);
+
+                if(isExistMuvelet != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        public bool IsExistMuveletByGUID(Muvelet muvelet)
+        {
+            using (var context = new MetaPlanContext())
+            {
+                Muvelet isExistMuvelet = context.Muveletek.Find(muvelet.GUID);
+
+                if (isExistMuvelet != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        public Muvelet GetByIdMuvelet(Muvelet muvelet)
+        {
+            using (var context = new MetaPlanContext())
+            {
+                return context.Muveletek.Find(muvelet.Id);
+                
+            }
+
+        }
+
+        public void UpdateMuveletById(Muvelet updatedMuvelet)
+        {
+            using (var context = new MetaPlanContext())
+            {
+                var muvelet = context.Muveletek.Find(updatedMuvelet.Id);
+                muvelet.Valasz = updatedMuvelet.Valasz;
+                muvelet.GUID = updatedMuvelet.GUID;
+                muvelet.TurelmiIdo = updatedMuvelet.TurelmiIdo;
+                muvelet.Keres = updatedMuvelet.Keres;
+                muvelet.ArgumentumLista = updatedMuvelet.ArgumentumLista;
+                muvelet.Nevproblemas = updatedMuvelet.Nevproblemas;
+                muvelet.ValaszTipus = updatedMuvelet.ValaszTipus;
+                context.SaveChanges();
+
+            }
+
+        }
+
+        public void UpdateMuveletByGUID(Muvelet updatedMuvelet)
+        {
+            using (var context = new MetaPlanContext())
+            {
+                var muvelet = context.Muveletek.Find(updatedMuvelet.GUID);
+                muvelet.Valasz = updatedMuvelet.Valasz;
+                muvelet.GUID = updatedMuvelet.GUID;
+                muvelet.TurelmiIdo = updatedMuvelet.TurelmiIdo;
+                muvelet.Keres = updatedMuvelet.Keres;
+                muvelet.ArgumentumLista = updatedMuvelet.ArgumentumLista;
+                muvelet.Nevproblemas = updatedMuvelet.Nevproblemas;
+                muvelet.ValaszTipus = updatedMuvelet.ValaszTipus;20.4
+                context.SaveChanges();
+
             }
 
         }
@@ -171,89 +121,7 @@ namespace CSARMetaPlanDBCap
                     .FirstOrDefault<Muvelet>(muvelet => muvelet.GUID == guid);
             }
         }
-
-        public void SaveProgramkod(Programkod programkod)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Programkodok.Add(programkod);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveRAMetaObjektum(RAMetaObjektum rAMetaObjektum)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.RAMetaObjektumok.Add(rAMetaObjektum);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveTabla(Tabla tabla)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Tablak.Add(tabla);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveTablaOszlop(TablaOszlop tablaOszlop)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.TablaOszlopok.Add(tablaOszlop);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveTaroltEljaras(TaroltEljaras taroltEljaras)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.TaroltEljarasok.Add(taroltEljaras);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveTaroltEljarasArgumentum(TaroltEljarasArgumentum taroltEljarasArgumentum)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.TaroltEljarasArgumentumok.Add(taroltEljarasArgumentum);
-                context.SaveChanges();
-            }
-
-
-        }
-
-        public void SaveTeszteles(Teszteles teszteles)
-        {
-            using (var context = new MetaPlanContext())
-            {
-
-                context.Tesztelesek.Add(teszteles);
-                context.SaveChanges();
-            }
-
-        }
+        
     }
 }
 
